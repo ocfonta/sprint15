2edb8f59b5fe577ffce2185345aed6f78188e65a
Luba <luponom@mail.ru>
2020-06-24 08:42:58 +0300

    node_env add

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,22 @@
 const jwt = require('jsonwebtoken');
 const AuthErr = require('../errors/unauthorized-err');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const handleAuthError = () => {
   throw new AuthErr('Необходима авторизация');
 };
 
 module.exports = (req, res, next) => {
-  const { jwt: token } = req.cookies;
-  if (!token) {
+  if (!req.cookies.jwt) {
+    console.log(res.cookies);
     return handleAuthError(res);
   }
+  const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return handleAuthError(res);
   }
